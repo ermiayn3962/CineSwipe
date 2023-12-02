9a08e5043d30c81cc8b8eb77a72000caad55f067
coopergolemme <coopergolemme@gmail.com>
2023-12-02 17:07:30 -0500

    apistuff

--- views/homepage.js
+++ views/homepage.js
@@ -22,68 +22,56 @@ function closeSidebar() {
   }
 }
 
-// function unzeroize() {
-//   document.getElementById("sidebar").style.width = "250px";
-//   document.getElementById("sidebar").style.padding = "15px";
+let kieranKey = "8ea8d568";
+let t = "totoro";
+let requestLink = "http://www.omdbapi.com/?apikey=" + kieranKey + "&t=" + t;
+/* ^ fetch input stuff ^ */
 
-//   document.getElementById("x-out").style.fontSize = "30px";
+let allInfo = "";
 
-//   document.getElementById("subheader").style.fontSize = "30px";
+async function getInfo(file) {
+  let x = await fetch(file);
+  let y = await x.text();
+  allInfo = JSON.parse(y);
+}
 
-//   document.getElementById("empty").style.fontSize = "20px";
+let title;
+let poster;
+let year;
+let description;
 
-//   const thumbnails = document.getElementsByClassName("thumbnail");
-//   for (let i = 0; i < thumbnails.length; i++) {
-//     thumbnails[i].style.height = "160px";
-//     thumbnails[i].style.width = "120px";
-//   }
-//   const titles = document.getElementsByClassName("title");
-//   for (let i = 0; i < titles.length; i++) {
-//     titles[i].style.fontSize = "25px";
-//   }
-//   const years = document.getElementsByClassName("year");
-//   for (let i = 0; i < years.length; i++) {
-//     years[i].style.fontSize = "15px";
-//   }
-//   const genres = document.getElementsByClassName("genre");
-//   for (let i = 0; i < genres.length; i++) {
-//     genres[i].style.fontSize = "15px";
-//   }
-//   const blurbs = document.getElementsByClassName("blurb");
-//   for (let i = 0; i < blurbs.length; i++) {
-//     blurbs[i].style.borderBottom = "4px dotted white";
-//   }
-// }
+async function run() {
+  await getInfo(requestLink);
+  title = allInfo["Title"];
+  console.log(title);
+}
 
-// function zeroize() {
-//   document.getElementById("sidebar").style.width = "0px";
-//   document.getElementById("sidebar").style.padding = "0px";
+run();
 
-//   document.getElementById("x-out").style.fontSize = "0px";
+var tmdb_api_key = "e2978d1da72adcf778b7bc358679b035";
 
-//   document.getElementById("subheader").style.fontSize = "0px";
+const options = { method: "GET", headers: { accept: "application/json" } };
 
-//   document.getElementById("empty").style.fontSize = "0px";
+const genres_url =
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+  tmdb_api_key +
+  "&language=en-US";
 
-//   const thumbnails0 = document.getElementsByClassName("thumbnail");
-//   for (let i = 0; i < thumbnails0.length; i++) {
-//     thumbnails0[i].style.height = "0px";
-//     thumbnails0[i].style.width = "0px";
-//   }
-//   const titles0 = document.getElementsByClassName("title");
-//   for (let i = 0; i < titles0.length; i++) {
-//     titles0[i].style.fontSize = "0px";
-//   }
-//   const years0 = document.getElementsByClassName("year");
-//   for (let i = 0; i < years0.length; i++) {
-//     years0[i].style.fontSize = "0px";
-//   }
-//   const genres0 = document.getElementsByClassName("genre");
-//   for (let i = 0; i < genres0.length; i++) {
-//     genres0[i].style.fontSize = "0px";
-//   }
-//   const blurbs0 = document.getElementsByClassName("blurb");
-//   for (let i = 0; i < blurbs0.length; i++) {
-//     blurbs0[i].style.borderBottom = "0px";
-//   }
-// }
+const jsonstuff = fetch(genres_url, options)
+  .then((res) => res.json())
+  .then((json) => () => {
+    return json;
+  })
+  .catch((err) => console.error("error:" + err));
+
+console.log(jsonstuff);
+
+const url =
+  "&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28";
+var rl =
+  "https://api.themoviedb.org/3/discover/movie?api_key=" + tmdb_api_key + url;
+
+// fetch(rl, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error("error:" + err));
