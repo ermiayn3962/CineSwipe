0224b329c5a8ff35622b6867b3bc4fe4cc7f943a
coopergolemme <coopergolemme@gmail.com>
2023-12-09 16:56:28 -0500

    worked on adding stuff to homepage

--- views/homepage.js
+++ views/homepage.js
@@ -24,15 +24,16 @@ var tmdb_api_key = "e2978d1da72adcf778b7bc358679b035";
 
 const options = { method: "GET", headers: { accept: "application/json" } };
 
-const genres_url =
-  "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+const popular_url =
+  "https://api.themoviedb.org/3/discover/movie?api_key=" +
   tmdb_api_key +
-  "&language=en-US";
+  "&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+
 const images_url = "https://image.tmdb.org/t/p/w500/";
 
-async function getGenres() {
+async function getPopular() {
   try {
-    let response = await fetch(genres_url, options);
+    let response = await fetch(popular_url, options);
     let data = await response.json();
     console.log(data);
     return data;
@@ -40,32 +41,5 @@ async function getGenres() {
     console.error("error:" + err);
   }
 }
-
-async function getImages(json) {
-  console.log(json);
-  json.results.forEach((element) => {
-    console.log("Title:", element.title);
-    console.log("Poster Path:", images_url + element.poster_path);
-    console.log("Backdrop Path", images_url + element.backdrop_path + "\n");
-  });
-}
-
-async function run() {
-  // await getting all genres
-  const genres = await getGenres();
-  // genres[3] is comedy, this is where we do specialized queries
-  const filters = "&with_genres=" + genres.genres[3].id;
-  const movies_url =
-    "https://api.themoviedb.org/3/discover/movie?api_key=" +
-    tmdb_api_key +
-    filters;
-  // gets results from
-  fetch(movies_url, options)
-    .then((res) => res.json())
-    .then((json) => getImages(json))
-    .catch((err) => console.error("error:" + err));
-
-  return genres;
-}
-
-run();
+const data = await getPopular();
+data;
